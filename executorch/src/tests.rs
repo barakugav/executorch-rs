be90fc35304df5afacef848cb28cf35d35e1d7ed
Barak Ugav <barakugav@gmail.com>
2025-03-10 08:16:31 +0200

    Move safe crate from ./ to /executorch

--- executorch/src/tests.rs
+++ executorch/src/tests.rs
@@ -7,6 +7,8 @@ use std::path::{Path, PathBuf};
 #[cfg(feature = "std")]
 pub fn add_model_path() -> PathBuf {
     Path::new(&env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
         .join("examples")
         .join("models")
         .join("add.pte")
@@ -14,13 +16,18 @@ pub fn add_model_path() -> PathBuf {
 
 pub const ADD_MODEL_PATH_CSTR: &CStr = unsafe {
     CStr::from_bytes_with_nul_unchecked(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/examples/models/add.pte", "\0").as_bytes(),
+        concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/../examples/models/add.pte",
+            "\0"
+        )
+        .as_bytes(),
     )
 };
 
 #[repr(align(16))]
 struct AlignedBytes<const N: usize>([u8; N]);
 const ADD_MODEL_BYTES_ALIGNED: AlignedBytes<
-    { include_bytes!("../examples/models/add.pte").len() },
-> = AlignedBytes(*include_bytes!("../examples/models/add.pte"));
+    { include_bytes!("../../examples/models/add.pte").len() },
+> = AlignedBytes(*include_bytes!("../../examples/models/add.pte"));
 pub const ADD_MODEL_BYTES: &[u8] = &ADD_MODEL_BYTES_ALIGNED.0;
